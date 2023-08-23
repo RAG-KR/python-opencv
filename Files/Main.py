81bd159d02f3d702d66b854cb0a14e797e17aa6a
Raghav KR <raghav.cs21@bmsce.ac.in>
2023-08-24 01:48:19 +0530

    in progress day1

--- Files/Main.py
+++ Files/Main.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import cv2
 cap = cv2.VideoCapture(0)
@@ -18,12 +19,22 @@ for path in modePathList:
 # print(len(imgModeList)) test that weve imported all the imgs correctly
 
 
+"""load the encoded values"""
+print("loading encoded file ...")
+file = open('encodeFile.p' , 'rb')
+encodeListWithIds = pickle.load(file)
+file.close()
+encodeListKnown , studentIds = encodeListWithIds
+# print(studentIds)
+print(" encoded file loaded !!!")
+
+
 while True:
     success , img = cap.read()
     """the things inside the array are starting and ending points of the background 
     that we are using the ui basicaly"""
     imgBackground[162 :162+480 , 55:55+640] = img
-    imgBackground[44 :44+633 , 808:808+414] = imgModeList[1]
+    imgBackground[44 :44+633 , 808:808+414] = imgModeList[2]
     # cv2.imshow("webcam",img)
 
     cv2.imshow("face attandance",imgBackground)
